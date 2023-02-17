0133e19f42211ab2959bd44c916d30c65172bf93
bjp <bj11077@metabuild.co.kr>
2023-02-17 17:09:25 +0900

    Files to Zip

--- src/main/java/com/ex/mtp/filetr/FileTradeService.java
+++ src/main/java/com/ex/mtp/filetr/FileTradeService.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Service
 public class FileTradeService {
@@ -45,6 +49,46 @@ public class FileTradeService {
 //        }
         TradeDto dto = new TradeDto(file.getName(),buf);
         return dto;
+    }
+
+    public TradeDto fileDownloadResponseToZip(String path) throws IOException {
+        // directory path
+        File dir = new File(path);
+        TradeDto dto = new TradeDto();
+        File[] files;
+        if(!dir.exists()){
+            return null;
+        }else if((files = dir.listFiles()).length == 0 ){
+            return null;
+        }
+        String zipName = "complete.zip";
+        String filePath = path+File.separator+zipName;
+        FileOutputStream outputStream = new FileOutputStream(filePath);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
+        FileInputStream inputStream = null;
+        for (File file : files) {
+            inputStream = new FileInputStream(file);
+            ZipEntry zipEntry = new ZipEntry(file.getName());
+            zipOutputStream.putNextEntry(zipEntry);
+            byte[] bytes = new byte[1024];
+            int length;
+            while((length = inputStream.read(bytes)) >= 0){
+                zipOutputStream.write(bytes,0,length);
+            }
+            inputStream.close();
+            zipOutputStream.closeEntry();
+        }
+        zipOutputStream.close();
+        outputStream.close();
+        dto.setFileName(zipName);
+        dto.setFile(fileToByteArray(filePath));
+        File delFile = new File(filePath);
+        delFile.delete();
+        return dto;
+    }
+
+    public byte[] fileToByteArray(String path) throws IOException {
+        return Files.readAllBytes(new File(path).toPath());
     }
 
 }
